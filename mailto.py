1757c1600a6a03541a5b8edb1373a0815ba5ec2c
Eric Yang <jiwen624@foxmail.com>
2016-04-28 15:09:22 -0400

    Update mailto.py
    -Added some parameters.

--- mailto.py
+++ mailto.py
@@ -1,47 +1,68 @@
 #!/usr/bin/env python3
+"""
+Send emails from command line (I use it for sending attachments directly from a server. :P)
+Usage: see ./mailto -h
+"""
+
+import os
+import sys
+import getpass
+import smtplib
+import argparse
 from email import encoders
 from email.header import Header
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import parseaddr, formataddr
 from email.mime.base import MIMEBase
-import getpass
-import smtplib
+
+try:
+    from pprint import pprint as _print
+except ImportError:
+    _print = print
+
 
 def format_header(s):
     name, addr = parseaddr(s)
     return formataddr((Header(name, 'utf-8').encode(), addr))
 
-#Parameters:
-#   - to: the receiver
-#   - subject: the subject of the mail
-#   - fileiter: an iterator of the file names as attachments
 
-def sendmail(parms):
-    #Hard-coded parameters
-    password = getpass.getpass()
-    smtp_server = 'smtp.example.com' #TODO
-    from_addr = 'sender@example.com'
-    cc = 'cc@example.com'
-    smtp_port = 587
+def send_mail(args):
+    """Send emails.
 
-    from_str = parms['from']
-    to = parms['to']
-    subject = parms['subject']
-    fileiter = parms['fileiter']
-    text = parms['text']
+    :param args:
+    - to: the receiver
+    - subject: the subject of the mail
+    - files: an iterator of the file names as attachments
 
-    #Pack email header
+    :return: None
+
+    """
+
+    password = getpass.getpass('Enter your password: ')
+    smtp_server = args['smtp_server']
+    sender = args['sender']
+    cc = ''
+    smtp_port = args['smtp_port']
+    to = args['to']
+    subject = args['subject']
+    files = args['attachments']
+    body = args['body']
+
+    # Pack email header
     msg = MIMEMultipart()
-    msg['From'] = format_header('%s <%s>' % (from_str, from_addr))
-    msg['To'] = format_header('Receiver <%s>' % to)
-    msg['Bcc'] = format_header('Me <%s>' % cc)
+    msg['From'] = format_header('{}'.format(sender))
+    msg['To'] = format_header('{}'.format(to))
     msg['Subject'] = subject
 
-    #Pack email contents and attachments. All the files in the path will be attached in the email.
-    msg.attach(MIMEText(text, 'plain', 'utf-8'))
+    if cc:
+        msg['Bcc'] = format_header('Me <{}>'.format(cc))
 
-    for file in fileiter:
+    # Pack email contents and attachments.
+    # All the files in the path will be attached in the email.
+    msg.attach(MIMEText(body, 'plain', 'utf-8'))
+
+    for file in files:
         with open(file, 'rb') as fp:
             mime = MIMEBase('application', 'octet-stream', filename=file.split('/')[-1])
             mime.add_header('Content-Disposition', 'attachment', filename=file.split('/')[-1])
@@ -51,43 +72,94 @@ def sendmail(parms):
             encoders.encode_base64(mime)
             msg.attach(mime)
 
+    # Sending the email
+    if cc:
+        to.append(cc)
 
-    #Sending the email
-    to = [to, cc] 
     server = smtplib.SMTP(smtp_server, smtp_port)
-    server.set_debuglevel(1)
-    server.login(from_addr, password)
-    server.sendmail(from_addr, to, msg.as_string())
+    if args['verbose']:
+        server.set_debuglevel(1)
+
+    server.login(sender, password)
+    server.sendmail(sender, to, msg.as_string())
     server.quit()
 
+    return 0
+
+
+def valid_email(s):
+    if '@' in s:
+        return s
+    else:
+        msg = "Not a valid email address: '{0}'.".format(s)
+        raise argparse.ArgumentTypeError(msg)
+
+
+def main():
+    """The main function to parse arguments and invoke the send_mail function.
+    """
+
+    parser = argparse.ArgumentParser(description="The Utility for Sending Emails On a Server")
+
+    parser.add_argument("sender", help="the sender, e.g, sender@example.com", type=valid_email)
+    parser.add_argument("recipients", help="the recipients separated by semicolons", type=valid_email)
+
+    parser.add_argument("-S", help="the smtp server", metavar='')
+    parser.add_argument("-P", help="the smtp port", metavar='', default='25')
+    parser.add_argument("-s", help="the subject of the email", metavar='')
+    parser.add_argument("-a", help="the file(s) to be attached, separated by spaces", metavar='', nargs='*')
+    parser.add_argument("-b", help="message body reading from a file or standard input: - ",
+                        type=argparse.FileType('r'), metavar='')
+    parser.add_argument("-B", help="message body by a string", metavar='')
+
+    parser.add_argument("-v", help="detailed print", action='store_true')
+
+    args = parser.parse_args()
+
+    files = []
+    paths = args.a if args.a else []
+
+    for attach_path in paths:
+        if os.path.isdir(attach_path):
+            for f in os.listdir(attach_path):
+                full_path = os.path.join(attach_path, f)
+                if os.path.isfile(full_path):
+                    files.append(full_path)
+        elif os.path.isfile(attach_path):
+            files.append(attach_path)
+        else:
+            pass
+
+    body = args.B if args.B else ''
+    body += args.b.read() if args.b else ''
+
+    sender = args.sender
+
+    if args.S:
+        smtp_server = args.S
+    else:
+        smtp_server = 'smtp.{}'.format(sender.split('@')[1])
+
+    smtp_port = args.P if args.P else '587'
+
+    verbose = args.v
+    params = {
+        'sender': sender,
+        'smtp_server': smtp_server,
+        'smtp_port': smtp_port,
+        'to': args.recipients,
+        'subject': args.s,
+        'body': body,
+        'attachments': files,
+        'verbose': verbose
+    }
+
+    if verbose:
+        _print(params)
+
+    send_mail(params)
+    print('Sent.')
+    return 0
 
 if __name__ == '__main__':
-    #Read input parameters and assert
-    import os
-    import sys
-
-    if len(sys.argv) != 4:
-        print('Usage: mailto [path_to_attach] [mail_recipients] [mail_subject]')
-        exit(1)
-
-    dir = sys.argv[1]
-    to = sys.argv[2]
-    subject = sys.argv[3] 
-    
-    if os.path.isdir(dir):
-        files = (os.path.join(dir, f) for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f)))
-    elif os.path.isfile(dir):
-        files = [dir] 
-    else:
-        print('Error: not a valid path.')
-        exit(1)    
-
-    parms = {
-        'from': 'Eric Yang',
-        'to': to,
-        'subject': subject,
-        'text': 'Please see attached files.',
-        'fileiter': files
-    }
-    sendmail(parms)
-    exit(0)
+    sys.exit(main())
